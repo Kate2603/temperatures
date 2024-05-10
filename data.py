c25445f2362f80d471634d11c88d7f2de451fd6c
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2024-05-10 12:39:15 +0300

    add file main, data and processing

---
+++ data.py
@@ -0,0 +1,9 @@
+def load_data(filename: str) -> list[str]:
+    with open(filename, "r") as file:
+        return file.readlines()
+
+def clean_data(temperature_data: list[str]) -> list[float]:
+    return new_func(temperature_data)
+
+def new_func(temperature_data):
+    return [float(temp.strip()) for temp in temperature_data if temp.strip()]
